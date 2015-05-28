3ddb60176631b7c11daec82423ba881dc3b2aed0
zhongxingdou <zhongxingdou@gmail.com>
2015-05-28 14:56:06 +0800

    设置新闻发布日期的默认值

--- app/models/news.rb
+++ app/models/news.rb
@@ -3,6 +3,12 @@ class News < ActiveRecord::Base
     has_many :categories, :through => :sort_news, :source => :news_category
     
     resourcify
-
     accepts_nested_attributes_for :categories
+
+    after_initialize :assign_defaults_on_new, if: 'new_record?'
+
+    private
+    def assign_defaults_on_new
+        self.pubdate = DateTime.now
+    end
 end
